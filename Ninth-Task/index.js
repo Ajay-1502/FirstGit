6a013e0b4961e5368423f8e3c8e40135fabee3ce
Ajay-1502 <ahdesai40@gmail.com>
2025-03-18 10:17:10 +0530

    Feature : Implemented Local Storage Concept

---
+++ Ninth-Task/index.js
@@ -0,0 +1,6 @@
+function handleFormSubmit(event) {
+  event.preventDefault();
+  localStorage.setItem('username', event.target.username.value);
+  localStorage.setItem('email', event.target.email.value);
+  localStorage.setItem('phone', event.target.phone.value);
+}
